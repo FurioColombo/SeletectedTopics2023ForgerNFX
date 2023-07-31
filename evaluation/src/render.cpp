804c2ee4768bda1649cc5ea20e872220022b10bd
Marco Furio Colombo <marco.furio.colombo@gmail.com>
2023-07-31 16:32:56 +0200

    + render.cpp now able to deal with bigger block sizes

--- evaluation/src/render.cpp
+++ evaluation/src/render.cpp
@@ -12,11 +12,13 @@ http://bela.io
 #include <cmath>
 #include <iostream>
 #include "../Bela/libraries/Gui/Gui.h"
+#include <typeinfo>
 
 Gui gui;
 
 int N_FRAMES = 1;
-const int lstm_dim = 8;
+const int lstm_dim = 32;
+const int frame_size = 16;
 float gFrequency = 440.0;
 float gPhase;
 float gInverseSampleRate;
@@ -24,7 +26,10 @@ float gInverseSampleRate;
 // this is the definition of the actual 
 // model architecture, LSTMLayerT
 // in this case 1->8->1 lstm->dense (aka linear in torch)
-using ModelType = RTNeural::ModelT<float, 1, 1, RTNeural::LSTMLayerT<float, 1, lstm_dim>, RTNeural::DenseT<float, lstm_dim, 1>>;
+using ModelType = RTNeural::ModelT<float, frame_size, frame_size,
+    RTNeural::LSTMLayerT<float, frame_size, lstm_dim>,
+    RTNeural::DenseT<float, lstm_dim, frame_size>
+    >;
 ModelType gModel;
 
 void loadModel(std::ifstream& jsonStream, ModelType& model)
@@ -39,12 +44,15 @@ void loadModel(std::ifstream& jsonStream, ModelType& model)
     std::string prefix = "lstm.";
     // for LSTM layers, number of hidden = number of params in a hidden weight set
     // divided by 4
-    auto hidden_count = modelJson[prefix + ".weight_ih_l0"].size() / 4;
+    // auto hidden_count = modelJson[prefix + ".weight_ih_l0"].size() / 4;
     // assert that the number of hidden units is the same as this count
     // to ensure the json file we are importing matches the model we defined.
     RTNeural::torch_helpers::loadLSTM<float> (modelJson, prefix, lstm);
-  
+
     auto& dense = model.get<1>();
+    std::cout << "dense in size: " << dense.in_size << std::endl;
+    std::cout << "dense out size: " << dense.out_size << std::endl;
+
     // as per the lstm prefix, here the json needs a key prefixed with dense. 
     RTNeural::torch_helpers::loadDense<float> (modelJson, "dense.", dense);
 }
@@ -61,69 +69,73 @@ bool setup(BelaContext *context, void *userData)
     // load neural model using RTNeural
     auto modelFilePath = "model.json";
     std::cout << "Loading model from path: " << modelFilePath << std::endl;
+    std::cout << "test" << std::endl;
+
     std::ifstream jsonStream(modelFilePath, std::ifstream::binary);
     loadModel(jsonStream, gModel);
     gModel.reset();
 
     // now take the model for a test spin 
-    std::vector<float> inputs {1.0f, 2.0f, 3.0f, 4.0f};
-    std::vector<float> outputs {};
-    outputs.resize(inputs.size(), {});
+    std::cout << "testing model... " << modelFilePath << std::endl;
 
-    for(size_t i = 0; i < inputs.size(); ++i)
+    /*
+    std::vector<float> inputs { 0.1f, 0.2f, 0.3f, 0.4f, 
+                                0.1f, 0.2f, 0.3f, 0.4f, 
+                                0.1f, 0.2f, 0.3f, 0.4f, 
+                                0.1f, 0.2f, 0.3f, 0.4f
+                                };
+    */
+    float inputs[16] = {    0.1f, 0.2f, 0.3f, 0.4f, 
+                        0.2f, 0.3f, 0.4f, 0.5f, 
+                        0.5f, 0.6f, 0.7f, 0.8f, 
+                        0.9f, 0.8f, 0.7f, 0.6f
+                        };
+
+    // float* inputs = in;
+    std::cout << "input type: " << typeid(inputs).name() << std::endl;
+
+    // std::vector<float> outputs {};
+    // outputs.resize(inputs.size(), {});
+    auto outputs = gModel.forward(inputs);
+    auto& dense = gModel.get<1>();
+
+    //std::cout << "output type: " << typeid(outputs).name() << std::endl;
+    std::cout << "dense out type: " << typeid(dense.outs).name() << std::endl;
+
+    for(size_t i = 0; i < frame_size; ++i)
     {
-        outputs[i] = gModel.forward(&inputs[i]);
-        std::cout << "in " << inputs[i] << " out: " << outputs[i] << std::endl;
+        std::cout << "in: " << inputs[i] << std::endl;
+        //std::cout << "out: " << outputs << std::endl;
+        auto& dense = gModel.get<1>();
+        std::cout << "dense out: " << dense.outs[i] << std::endl;
+
     }
 	return true;
+    
 }
 
 void render(BelaContext *context, void *userData)
 {
+    
     unsigned int nFrames = context->audioFrames;
-    // std::vector<float> inputs {};
-    // std::vector<float> outputs {};
-    // input.resize(nFrames, {0});
-    // outputs.resize(nFrames, {0});
-    // float inputs[16];
-    // float outputs[16];
-    float input = 0;
-    float output = 0;
+
+    float inputs[nFrames] = { 0 };
+    float outputs[nFrames] = { 0 };
 
     // Read audio input
 	for(unsigned int n = 0; n < nFrames; n++) {
-        input = audioRead(context, n, 0);  
-
-        output = gModel.forward(&input);
-
-
-        audioWrite(context, n, 0, output);
-
-        //inputs[n] = audioRead(context, n, 0) * 0.5f;
-        //outputs[n] = gModel.forward(&inputs[n]);
-        //audioWrite(context, n, 0, outputs[n]);
-        // use this for stereo+ signals
-		// for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
-        // 
-		// }
+        inputs[n] = audioRead(context, n, 0);  
 	}
-    /*
-    // Process input through the model 
-    outputs = gModel.forward(inputs);
-    //    outputs[i] = model.forward(&inputs[i]);
-    //    std::vector<float> outputs {};
+    outputs[0] = gModel.forward(&inputs[0]);
 
+    auto& dense = gModel.get<1>();
 
-    // Write audio outputs
-	for(unsigned int m = 0; m < nFrames; m++) {
-        audioWrite(context, m, 0, outputs[m]);
-
-        // use this for stereo+ signals
-		// for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
-        // 
-		// }
+    // Write audio output
+	for(unsigned int n = 0; n < nFrames; n++) {
+        // audioWrite(context, n, 0, outputs[n]);
+        audioWrite(context, n, 0, dense.outs[n]);
 	}
-    */
+    // std::cout << "in: " << inputs[1] << " - out: " << dense.outs[1] << std::endl;
 }
 
 void cleanup(BelaContext *context, void *userData)
