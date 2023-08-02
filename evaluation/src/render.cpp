486a4c418df736063f732ef235a19332b65ae10e
Marco Furio Colombo <marco.furio.colombo@gmail.com>
2023-08-02 14:07:14 +0200

    render.cpp: + gui linked + bypass function + wet function - with optimization for big frame sizes sketch.js + working minimal gui for selecting different models t runtime

--- evaluation/src/render.cpp
+++ evaluation/src/render.cpp
@@ -1,29 +1,22 @@
-/*
- ____  _____ _        _
-| __ )| ____| |      / \
-|  _ \|  _| | |     / _ \
-| |_) | |___| |___ / ___ \
-|____/|_____|_____/_/   \_\
-http://bela.io
-*/
-
 #include <Bela.h>
+#include "../Bela/libraries/Gui/Gui.h"
 #include "../RTNeural/RTNeural/RTNeural.h"
 #include <cmath>
 #include <iostream>
-#include "../Bela/libraries/Gui/Gui.h"
 #include <typeinfo>
 
 Gui gui;
 
-int N_FRAMES = 1;
-const int lstm_dim = 32;
-const int frame_size = 16;
-float gFrequency = 440.0;
-float gPhase;
-float gInverseSampleRate;
+//const auto tubeScreamModelFilePath = "tube_screamer_model.json";
+const auto tubeScreamModelFilePath = "model.json";
+//const auto bluesDriverModelFilePath = "blues_driver_model.json";
+const auto bluesDriverModelFilePath = "model.json";
+const auto RATModelFilePath = "RAT_model.json";
 
-// this is the definition of the actual 
+const int lstm_dim = 6;
+const int frame_size = 1;
+
+// this is the definition of the actual
 // model architecture, LSTMLayerT
 // in this case 1->8->1 lstm->dense (aka linear in torch)
 using ModelType = RTNeural::ModelT<float, frame_size, frame_size,
@@ -31,112 +24,142 @@ using ModelType = RTNeural::ModelT<float, frame_size, frame_size,
     RTNeural::DenseT<float, lstm_dim, frame_size>
     >;
 ModelType gModel;
+ModelType tubeScreamerModel;
+ModelType bluesDriverModel;
+ModelType RATModel;
+ModelType models[3] = {tubeScreamerModel, bluesDriverModel, RATModel};
 
-void loadModel(std::ifstream& jsonStream, ModelType& model)
+
+void loadModel(ModelType& model, auto modelWeightsPaths)
 {
+    std::cout << "Loading model from path: " << modelWeightsPaths << std::endl;
+    std::ifstream jsonStream(modelWeightsPaths, std::ifstream::binary);
+
     nlohmann::json modelJson;
     jsonStream >> modelJson;
-
     auto& lstm = model.get<0>();
-    // note that the "lstm." is a prefix used to find the 
-    // lstm data in the json file so your python
-    // needs to name the lstm layer 'lstm' if you use lstm. as your prefix
     std::string prefix = "lstm.";
-    // for LSTM layers, number of hidden = number of params in a hidden weight set
-    // divided by 4
-    // auto hidden_count = modelJson[prefix + ".weight_ih_l0"].size() / 4;
-    // assert that the number of hidden units is the same as this count
-    // to ensure the json file we are importing matches the model we defined.
     RTNeural::torch_helpers::loadLSTM<float> (modelJson, prefix, lstm);
-
     auto& dense = model.get<1>();
     std::cout << "dense in size: " << dense.in_size << std::endl;
     std::cout << "dense out size: " << dense.out_size << std::endl;
 
-    // as per the lstm prefix, here the json needs a key prefixed with dense. 
+    // as per the lstm prefix, here the json needs a key prefixed with dense.
     RTNeural::torch_helpers::loadDense<float> (modelJson, "dense.", dense);
+    model.reset();
+}
+
+void render_bypass_fx(BelaContext *context, unsigned int nFrames, float isOn){
+    // bypass fx: in = out
+    float input = 0;
+	for(unsigned int n = 0; n < nFrames; n++) {
+        input = audioRead(context, n, 0);
+        audioWrite(context, n, 0, input*isOn);
+	}
+}
+
+void render_wet_fx(BelaContext *context, unsigned int nFrames, int model_idx, float isOn){
+
+    if(isOn == 0){
+        // Write audio output
+        for(unsigned int n = 0; n < nFrames; n++) {
+            audioWrite(context, n, 0, 0);
+        }
+        return;
+    }
+    if (frame_size==1){
+        float input;
+        float output;
+        for(unsigned int n = 0; n < nFrames; n++) {
+            input = audioRead(context, n, 0);
+            output = models[model_idx].forward(&input);
+            audioWrite(context, n, 0, output);
+        }
+    } else {
+        float inputs[nFrames] = { 0 };
+        float outputs[nFrames] = { 0 };
+        // Read audio input
+        for(unsigned int n = 0; n < nFrames; n++) {
+            inputs[n] = audioRead(context, n, 0);
+        }
+        outputs[0] = models[model_idx].forward(&inputs[0]);
+
+        auto& dense = models[model_idx].get<1>();
+
+        // Write audio output
+        for(unsigned int n = 0; n < nFrames; n++) {
+            audioWrite(context, n, 0, dense.outs[n]);
+        }
+    }
 }
 
 bool setup(BelaContext *context, void *userData)
-{   
-    // init gui
+{
+    // GUI
     gui.setup(context->projectName);
+	gui.setBuffer('f', 2);	//Set the buffer to receive from the GUI
 
     // Print out sample rate
     std::cout << "Sample Rate : " << context->audioSampleRate << std::endl;
     std::cout << "AudioFrames per block: " << context->audioFrames << std::endl;
 
-    // load neural model using RTNeural
-    auto modelFilePath = "model.json";
-    std::cout << "Loading model from path: " << modelFilePath << std::endl;
-    std::cout << "test" << std::endl;
+    // load neural models using RTNeural
+    // Tube Screamer model
+    loadModel(tubeScreamerModel, tubeScreamModelFilePath);
+    // Blues Driver Model
+    loadModel(bluesDriverModel, bluesDriverModelFilePath);
+    // RAT Model
+    loadModel(RATModel, RATModelFilePath);
 
-    std::ifstream jsonStream(modelFilePath, std::ifstream::binary);
-    loadModel(jsonStream, gModel);
-    gModel.reset();
+    // now take the models for a test spin
+    std::cout << "testing models... " << std::endl;
+    float inputs[16] = {
+        0.1f, 0.2f, 0.3f, 0.4f,
+        0.2f, 0.3f, 0.4f, 0.5f,
+        0.5f, 0.6f, 0.7f, 0.8f,
+        0.9f, 0.8f, 0.7f, 0.6f
+    };
 
-    // now take the model for a test spin 
-    std::cout << "testing model... " << modelFilePath << std::endl;
+    for(int j=0; j<3; j++) {
+        std::cout << "input type: " << typeid(inputs).name() << std::endl;
 
-    /*
-    std::vector<float> inputs { 0.1f, 0.2f, 0.3f, 0.4f, 
-                                0.1f, 0.2f, 0.3f, 0.4f, 
-                                0.1f, 0.2f, 0.3f, 0.4f, 
-                                0.1f, 0.2f, 0.3f, 0.4f
-                                };
-    */
-    float inputs[16] = {    0.1f, 0.2f, 0.3f, 0.4f, 
-                        0.2f, 0.3f, 0.4f, 0.5f, 
-                        0.5f, 0.6f, 0.7f, 0.8f, 
-                        0.9f, 0.8f, 0.7f, 0.6f
-                        };
+        // auto outputs = models[j].forward(inputs);
+        auto& dense = models[j].get<1>();
 
-    // float* inputs = in;
-    std::cout << "input type: " << typeid(inputs).name() << std::endl;
+        std::cout << "dense out type: " << typeid(dense.outs).name() << std::endl;
 
-    // std::vector<float> outputs {};
-    // outputs.resize(inputs.size(), {});
-    auto outputs = gModel.forward(inputs);
-    auto& dense = gModel.get<1>();
+        for(size_t i = 0; i < frame_size; ++i)
+        {
+            std::cout << i << " - in: " << inputs[i] << std::endl;
+            auto& dense = models[j].get<1>();
+            std::cout << "dense out: " << dense.outs[i] << std::endl;
 
-    //std::cout << "output type: " << typeid(outputs).name() << std::endl;
-    std::cout << "dense out type: " << typeid(dense.outs).name() << std::endl;
-
-    for(size_t i = 0; i < frame_size; ++i)
-    {
-        std::cout << "in: " << inputs[i] << std::endl;
-        //std::cout << "out: " << outputs << std::endl;
-        auto& dense = gModel.get<1>();
-        std::cout << "dense out: " << dense.outs[i] << std::endl;
-
+        }
     }
+    std::cout << "setup complete" << std::endl;
+
 	return true;
-    
 }
 
 void render(BelaContext *context, void *userData)
 {
-    
+    // GUI DataBuffer
+	DataBuffer& buffer = gui.getDataBuffer(0);
+	float* data = buffer.getAsFloat();
+    float isOn = (float) data[0];
+    int fxType = (int) data[1];
     unsigned int nFrames = context->audioFrames;
 
-    float inputs[nFrames] = { 0 };
-    float outputs[nFrames] = { 0 };
-
-    // Read audio input
-	for(unsigned int n = 0; n < nFrames; n++) {
-        inputs[n] = audioRead(context, n, 0);  
-	}
-    outputs[0] = gModel.forward(&inputs[0]);
-
-    auto& dense = gModel.get<1>();
-
-    // Write audio output
-	for(unsigned int n = 0; n < nFrames; n++) {
-        // audioWrite(context, n, 0, outputs[n]);
-        audioWrite(context, n, 0, dense.outs[n]);
-	}
-    // std::cout << "in: " << inputs[1] << " - out: " << dense.outs[1] << std::endl;
+    switch (fxType){
+        case 0:
+            render_bypass_fx(context, nFrames, isOn);
+            break;
+        default:
+            render_wet_fx(context, nFrames, fxType, isOn);
+    }
 }
+
+
 
 void cleanup(BelaContext *context, void *userData)
 {
--- evaluation/src/sketch.js
+++ evaluation/src/sketch.js
@@ -6,57 +6,68 @@ Bela.data.sendBuffer(0, 'float', buffer);
 where the first argument (0) is the index of the sent buffer, second argument ('float') is the type of the
 data, and third argument (buffer) is the data array to be sent.
 */
-const VOLUME_SLIDER_IDX = 0
+let offColor;
+let onColor;
 
-const CLEAN_BUTTON_IDX = 1
-const TUBESCREAMER_BUTTON_IDX = 2
-const BLUESDRIVER_BUTTON_IDX = 3
-const RAT_BUTTON_IDX = 4
+const BUFFER_ONOFF_IDX = 0
+const BUFFER_ACTIVE_FX_IDX = 1
+
+const CLEAN_BUTTON_IDX = 0
+const TUBESCREAMER_BUTTON_IDX = 1
+const BLUESDRIVER_BUTTON_IDX = 2
+const RAT_BUTTON_IDX = 3
 
 //buffer to send to Bela. 5 elements:
 // 4 buttons [clean, TubeScreamer, BluesDriver, RAT]
 // 1 slider [Volume]
-let buffer = [0, 0, 0, 0];
-//current state of the PLAY/STOP button.
-let buttonStates = [1, 0, 0, 0];
+// buffer: [isFxOn, activeFXButton]
+let buffer = [1, 0];
+let activeFXButton = 0;
+let isFXOn = 0;
 let buttonNames = ['Clean', 'TubeScreamer', 'BluesDriver', 'RAT']
 
 function setup() {
-	//Create a thin canvas where to allocate the elements (this is not strictly neccessary because
-	//we will use DOM elements which can be allocated directly in the window)
-//	createCanvas(windowWidth * 3/4, windowHeight / 5);
 	createCanvas(windowWidth, windowHeight);
 
-	//Create two sliders, first to control frequency and second to control amplitude of oscillator
-	//both go from 0 to 100, starting with value of 60
-    volumeSliderMin = 0
-    volumeSliderMax = 100
-    volumeSliderInitial = 80
-	volumeSlider = createSlider(volumeSliderMin, volumeSliderMax, volumeSliderInitial);
+    offColor = color(0, 10, 10, 100);
+    onColor = color(50, 110, 110, 110);
 
-	//Create Distorsion type buttons
+    // Create buttons
+    // Create On/Off button
+    onOffButton = createButton("ON/OFF");
+	// Create Distorsion type buttons
 	cleanButton = createButton("Clean");
-	cleanButton.mouseClicked( function(){
-         changeButtonState(CLEAN_BUTTON_IDX, cleanButton);
+	tubeScreamerButton = createButton("Tube\nScreamer");
+	bluesDriverButton = createButton("Blues\nDriver");
+	RATButton = createButton("RAT");
+    let distorsionTypeBtns = [
+        cleanButton,
+        tubeScreamerButton,
+        bluesDriverButton,
+        RATButton
+    ]
+
+	// On/Off Button listener
+    onOffButton.mouseClicked( function(){
+        changeOnOffButtonState(isFXOn, onOffButton);
+       }
+    );
+
+	// Distorsion type buttons listeners
+    cleanButton.mouseClicked( function(){
+        changeDistorsionTypeButtonState(CLEAN_BUTTON_IDX, distorsionTypeBtns);
+       }
+    );
+    tubeScreamerButton.mouseClicked( function(){
+        changeDistorsionTypeButtonState(TUBESCREAMER_BUTTON_IDX, distorsionTypeBtns);
         }
     );
-
-    //Create Distorsion type buttons
-	tubeScreamerButton = createButton("Tube\nScreamer");
-	tubeScreamerButton.mouseClicked( function(){
-        changeButtonState(TUBESCREAMER_BUTTON_IDX, tubeScreamerButton);
+    bluesDriverButton.mouseClicked( function(){
+        changeDistorsionTypeButtonState(BLUESDRIVER_BUTTON_IDX, distorsionTypeBtns);
        }
     );
-    //Create Distorsion type buttons
-	bluesDriverButton = createButton("Blues\nDriver");
-	bluesDriverButton.mouseClicked( function(){
-        changeButtonState(BLUESDRIVER_BUTTON_IDX, bluesDriverButton);
-       }
-    );
-    //Create Distorsion type buttons
-	RATButton = createButton("RAT");
-	RATButton.mouseClicked( function(){
-        changeButtonState(RAT_BUTTON_IDX, RATButton);
+    RATButton.mouseClicked( function(){
+        changeDistorsionTypeButtonState(RAT_BUTTON_IDX, distorsionTypeBtns);
        }
     );
 	//This function will format colors and positions of the DOM elements (sliders, button and text)
@@ -67,29 +78,40 @@ function draw() {
     background(5,5,255,1);
 
     //store values in the buffer
-	buffer[VOLUME_SLIDER_IDX]       =   volumeSlider.value()/100;
-	buffer[CLEAN_BUTTON_IDX]        =   buttonStates[CLEAN_BUTTON_IDX];
-    buffer[TUBESCREAMER_BUTTON_IDX] =   buttonStates[TUBESCREAMER_BUTTON_IDX];
-    buffer[BLUESDRIVER_BUTTON_IDX]  =   buttonStates[BLUESDRIVER_BUTTON_IDX];
-    buffer[RAT_BUTTON_IDX]          =   buttonStates[RAT_BUTTON_IDX];
+	buffer[BUFFER_ONOFF_IDX] = isFXOn;
+	buffer[BUFFER_ACTIVE_FX_IDX] = activeFXButton;
 
 	//SEND BUFFER to Bela. Buffer has index 0 (to be read by Bela),
 	//contains floats and sends the 'buffer' array.
-    // Bela.data.sendBuffer(0, 'float', buffer);
+    Bela.data.sendBuffer(0, 'float', buffer);
 }
 
 function formatDOMElements() {
 
 	//Format Buttons and labels
+    //ON/OFF button
+    onOffButtonsDimension = 50
+    onOffButtonsYpos = windowHeight/2 - onOffButtonsDimension/2
+    btnDistance = 125
+
+    //fx buttons
     fxButtonsDimension = 100
     fxButtonsYpos = windowHeight/2 - fxButtonsDimension/2
     btnDistance = 125
 
-    // Clean Button
+    // ON/OFF Button
     posX = fxButtonsDimension
+	onOffButton.position(posX, onOffButtonsYpos);
+	onOffButton.size(onOffButtonsDimension, onOffButtonsDimension);
+    setDefaultFxButtonStyle(onOffButton)
+
+    // Clean Button
+    posX = fxButtonsDimension + btnDistance
 	cleanButton.position(posX, fxButtonsYpos);
 	cleanButton.size(fxButtonsDimension, fxButtonsDimension);
     setDefaultFxButtonStyle(cleanButton)
+    cleanButton.style('background-color', onColor);
+
 
     // Tube Screamer Button
     posX = posX + btnDistance
@@ -108,40 +130,33 @@ function formatDOMElements() {
 	RATButton.position(posX, fxButtonsYpos);
 	RATButton.size(fxButtonsDimension, fxButtonsDimension);
     setDefaultFxButtonStyle(RATButton)
-
-    // Format sliders
-    // Volume slider
-    sliderHeight = 120
-    sliderWidth = 20
-    posX = posX + 200
-    let back_col = color(0, 10, 10, 100);
-
-	volumeSlider.position(posX, windowHeight/2);
-    volumeSlider.size(sliderHeight, sliderWidth);
-    volumeSlider.style("transform", "rotate(-90deg)");
 	}
 
 //Function that changes buttonState variable and changes button's background-color
-function changeButtonState(buttonIdx, button) {
-    let off_col = color(0, 10, 10, 100);
-    let on_col = color(50, 110, 110, 100);
-    if (buttonStates[buttonIdx] == 0){
-        buttonStates[buttonIdx] = 1
-        button.style('background-color', on_col);
+function changeDistorsionTypeButtonState(buttonIdx, buttons) {
+    activeFXButton = buttonIdx;
 
-    } else {
-        buttonStates[buttonIdx] = 0
-        button.style('background-color', off_col);
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style('background-color', offColor);
     }
-
+    buttons[buttonIdx].style('background-color', onColor);
 }
 
+//Function that changes buttonState variable and changes button's background-color
+function changeOnOffButtonState(buttonIdx, button) {
+    if (isFXOn) {
+        button.style('background-color', offColor);
+        isFXOn = 0;
+    } else {
+        button.style('background-color', onColor);
+        isFXOn = 1;
+    }
+}
 
 //Function that changes buttonState variable and changes button's background-color
 function setDefaultFxButtonStyle(button) {
 	let col = color(0, 10, 10, 100);
 	button.style('font-weight','bolder');
-	//button.style('background-image','url(https://fg-a.com/music/maracas-animation-2018.gif)');
 	button.style('border', '2px solid #000000');
 	button.style('border-radius', '50%');
     button.style('background-color', col);
